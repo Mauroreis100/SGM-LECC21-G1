458e85cb3ec05f7c53d40dcab1adb12ac43f748f
Mauro Reis <69197927+Mauroreis100@users.noreply.github.com>
2023-10-22 17:06:23 +0200

    1st commit

---
+++ SGM_LECC21_G1/src/interfaces/Operacoes.java
@@ -0,0 +1,22 @@
+package interfaces;
+
+import java.util.Vector;
+
+public interface Operacoes {
+//	public static Vector criar(Object a) {
+//		return a;
+//
+//	}
+//	public static Vector actualizar(Object a) {
+//	return a;
+//
+//}
+//	public static Vector eliminar(Object a) {
+//	return a;
+//
+//}
+//	public static Vector editar(Object a) {
+//	return a;
+//
+//}
+}
---
+++ SGM_LECC21_G1/src/inventario/Inventario.java
@@ -0,0 +1,5 @@
+package inventario;
+
+public class Inventario {
+
+}
