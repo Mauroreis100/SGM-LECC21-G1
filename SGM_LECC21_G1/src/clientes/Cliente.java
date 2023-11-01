874d6ceeb427f0866fd1d124710e306cc2546711
WalterPHD <WalterS4lg4d0@gmail.com>
2023-11-01 20:53:31 +0200

    Cliente(Sem registo no vector)

---
+++ SGM_LECC21_G1/src/clientes/Cliente.java
@@ -0,0 +1,75 @@
+package clientes;
+
+public class Cliente {
+	private String nome;
+	private int id ;
+	private String BI,cell;
+	private double saldo;
+	
+	public Cliente() {}
+	public Cliente( int id,String nome, String BI, String cell, double saldo) {
+		super();
+		this.nome = nome;
+		this.id = id;
+		this.BI = BI;
+		this.cell = cell;
+		this.saldo = saldo;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getBI() {
+		return BI;
+	}
+
+
+	public void setBI(String BI) {
+		BI = BI;
+	}
+
+
+	public String getCell() {
+		return cell;
+	}
+
+
+	public void setCell(String cell) {
+		this.cell = cell;
+	}
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", id=" + id + ", BI=" + BI + ", cell=" + cell + ", saldo=" + saldo + "]";
+	}
+	
+}
--- SGM_LECC21_G1/src/users/Cliente.java
+++
@@ -1,7 +0,0 @@
-package users;
-
-public class Cliente {
-	private String nome;
-	private int id ;
-	private double saldo;
-}
