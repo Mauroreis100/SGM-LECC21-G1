78bb997c23e44a42c994f482cbfc4a770388acfb
telesshelton <stelesu21@gmail.com>
2023-11-06 10:31:32 +0200

    Nova alterações

---
+++ SGM_LECC21_G1/src/armazem/Armazem.java
@@ -0,0 +1,25 @@
+package armazem;
+
+public class Armazem {
+
+	private String nome;
+	private int id ;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+
+}
